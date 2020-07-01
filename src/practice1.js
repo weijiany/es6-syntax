b2df3e9bad0d84758d8624fb8d7f53abfc93d969
weijian <weijian.yang@thoughtworks.com>
2020-07-01 18:29:41 +0800

    add implement

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = function ({ data, column }) {
+    let columnNames = column.map(c => c.name);
+    return data.map(row =>
+        Object.assign({}, ... Object.keys(row).map(index =>
+            ({
+                [columnNames[index]]: row[index]
+            }))
+        )
+    );
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,18 @@
-const inject
-export { inject };
+const inject = function (items, sections) {
+    let itemsWithNullAsFirstItem = [null, ...items];
+    return sections.map(section => ({
+        ...section,
+        previousItem: itemsWithNullAsFirstItem[section.index + 1]
+    })).reduce((items, section) => {
+        let index = items.findIndex(item => section.previousItem === item);
+        return insert(items, section.content, index)
+    }, itemsWithNullAsFirstItem).slice(1)
+}
+
+const insert = function (arr, item, index) {
+    let left = arr.slice(0, index);
+    let right = arr.slice(index, arr.length);
+    return [...left, item, ...right];
+}
+
+export { inject, insert };
